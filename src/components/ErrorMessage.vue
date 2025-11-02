abbc22ef1c11af4e88d806918ef9183cc34cc3aa
Bashar <basharabulaziz25@gmail.com>
2025-11-02 22:17:20 +0300

    Error message and Loading spinner components added

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  message: string
+}>()
+</script>
+
+<template>
+  <div class="rounded-lg bg-red-100 p-4 text-red-700">
+    <p class="font-semibold">Error</p>
+    <p>{{ message }}</p>
+  </div>
+</template>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center">
+    <div
+      class="h-12 w-12 rounded-full animate-spin border-4 border-blue-500 border-t-transparent"
+    ></div>
+  </div>
+</template>
