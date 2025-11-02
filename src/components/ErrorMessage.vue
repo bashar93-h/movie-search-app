3d1bf353e40543492c9153d9af0cc58d4570aa2a
Bashar <basharabulaziz25@gmail.com>
2025-11-02 23:57:30 +0300

    Error message file updates

--- src/components/ErrorMessage.vue
+++ src/components/ErrorMessage.vue
@@ -6,7 +6,7 @@ const props = defineProps<{
 
 <template>
   <div class="rounded-lg bg-red-100 p-4 text-red-700">
-    <p class="font-semibold">Error</p>
+    <p class="font-semibold">Error!</p>
     <p>{{ props.message }}</p>
   </div>
 </template>
