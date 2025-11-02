5f3c0d02b90f61b3e6744add2683bb9b188057ab
Bashar <basharabulaziz25@gmail.com>
2025-11-02 22:09:55 +0300

    movie store updated to use TS interfaces

--- src/stores/movieStore.ts
+++ src/stores/movieStore.ts
@@ -1,24 +1,58 @@
 import { defineStore } from 'pinia'
-
-interface Movie {
-  imdbID: string
-  title: string
-  year?: string
-  poster?: string
-}
+import { Movie, MovieDetails } from '@/types/movie'
+import { movieService } from '@/services/movieService'
 
 export const useMovieStore = defineStore('movieStore', {
   state: () => ({
     movies: [] as Movie[],
+    selectedMovie: null as MovieDetails | null,
     favorites: [] as Movie[],
+    loading: false,
+    error: null as string | null,
   }),
 
   actions: {
-    setMovies(data: Movie[]) {
-      this.movies = data
+    async searchMovie(query: string) {
+      this.loading = true
+      this.error = null
+
+      try {
+        const response = await movieService.searchMovies(query)
+        if (response.Response === 'True') {
+          this.movies = response.Search
+        } else {
+          this.error = 'No movies found'
+          this.movies = []
+        }
+      } catch (e) {
+        this.error = 'Failed to search movies'
+        console.error(e)
+      } finally {
+        this.loading = false
+      }
     },
+
+    async fetchMovieDetails(imdbID: string) {
+      this.loading = true
+      this.error = null
+
+      try {
+        this.selectedMovie = await movieService.getMovieDetails(imdbID)
+      } catch (e) {
+        this.error = 'Failed to fetch movie details'
+        console.error(e)
+      } finally {
+        this.loading = false
+      }
+    },
+
     addFavorite(movie: Movie) {
-      if (!this.favorites.some((f) => f.imdbID === movie.imdbID)) this.favorites.push(movie)
+      if (!this.favorites.some((f) => f.imdbID === movie.imdbID)) {
+        this.favorites.push(movie)
+      }
+    },
+    removeFavorite(imdbID: string) {
+      this.favorites = this.favorites.filter((f) => f.imdbID !== imdbID)
     },
   },
 })
