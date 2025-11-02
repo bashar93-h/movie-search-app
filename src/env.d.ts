ce6fd5f9f26c640e920cfdd67ddde7a371b527f6
Bashar <basharabulaziz25@gmail.com>
2025-11-02 22:04:24 +0300

    .env and TS definitions for environment variables added

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,14 @@
+// TS defenition file
+// TypeScript doesn't natively understand:
+// 1.vue files
+// 1.Vite's special import.meta features
+// 1.doesn’t automatically know about environment variables.
 /// <reference types="vite/client" />
 
 // Vite environment variables
 interface ImportMetaEnv {
+  readonly VITE_OMDB_API_KEY: string
+  readonly VITE_API_BASE_URL: string
   readonly BASE_URL: string
   readonly MODE: string
   readonly DEV: boolean
