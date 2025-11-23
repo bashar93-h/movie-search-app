391e4111d9e8d314aa75ba9d140eecc2d50d386b
Bashar <basharabulaziz25@gmail.com>
2025-11-23 22:02:41 +0300

    update Error-message and loading-spinner components

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center mt-4">
     <div
       class="h-12 w-12 rounded-full animate-spin border-4 border-blue-500 border-t-transparent"
     ></div>
