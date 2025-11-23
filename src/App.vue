194c1d589471056da8e7dffeba0c0f2e3540b352
Bashar <basharabulaziz25@gmail.com>
2025-11-23 22:00:35 +0300

    update App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,10 @@
 <script setup lang="ts">
-//adding comments //adds
-import { onMounted } from 'vue'
-import { useLocalStorage } from './composables/useLocalStorage'
-import LoadingSpinner from './components/LoadingSpinner.vue'
-import ErrorMessage from './components/ErrorMessage.vue'
-import { useToast } from 'vue-toastification'
-const toast = useToast()
-
-const darkMode = useLocalStorage('dark-mode', false)
-const toggleDarkMode = () => {
-  darkMode.value = !darkMode.value
-  darkMode.value
-    ? document.documentElement.classList.add('dark')
-    : document.documentElement.classList.remove('dark')
-}
-onMounted(() => {
-  darkMode.value
-    ? document.documentElement.classList.add('dark')
-    : document.documentElement.classList.remove('dark')
-})
-
-function handleClick() {
-  toast.success('Search completed')
-}
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl bg-white text-black dark:bg-background-dark dark:text-text-dark">
-    Movie search app
-  </h1>
+  <Header />
   <RouterView />
-  <button @click="toggleDarkMode">
-    {{ darkMode ? '☀️' : '🌙' }}
-  </button>
-
-  <LoadingSpinner />
-  <ErrorMessage :message="'movie cannot be loaded'" />
-
-  <button @click="handleClick">search</button>
 </template>
 
 <style scoped></style>
