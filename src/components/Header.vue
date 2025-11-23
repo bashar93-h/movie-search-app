af57e6c8301ce951a1f106c0a9b00e00280711cf
Bashar <basharabulaziz25@gmail.com>
2025-11-23 21:41:50 +0300

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import MainNav from './MainNav.vue'
+import SearchInput from './SearchInput.vue'
+</script>
+
+<template>
+  <header class="bg-primary-light dark:bg-primary-dark transition-colors duration-500">
+    <div class="max-w-7xl mx-auto flex justify-between items-center">
+      <div class="h-[72px] w-auto flex items-center">
+        <RouterLink to="/">
+          <img src="/logo.png" alt="Movie search app logo" class="h-[50px]" />
+        </RouterLink>
+      </div>
+      <SearchInput />
+      <MainNav />
+    </div>
+  </header>
+</template>
