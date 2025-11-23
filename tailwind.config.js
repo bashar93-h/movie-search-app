49421eda9d0d5b9ea814476f5a3dc56894fe4db8
Bashar <basharabulaziz25@gmail.com>
2025-11-23 22:07:07 +0300

    update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,31 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        goldman: ['Goldman', 'sans-serif'],
+      },
       colors: {
         primary: {
-          light: '#1E293B',
-          dark: '#334155',
+          light: '#1976D2',
+          dark: '#1F1F1F',
+        },
+        secondary: {
+          light: '#F0F0F0',
+          dark: '#1E1E1E',
         },
         accent: {
           light: '#C59D5F',
           dark: '#FACC15',
         },
         background: {
-          light: '#FAFAFA',
-          dark: '#0F172A',
+          light: '#F5F5F5',
+          dark: '#121212',
         },
         text: {
-          light: '#111827',
-          dark: '#E2E8F0',
+          light: '#000000',
+          dark: '#FFFFFF',
         },
       },
     },
