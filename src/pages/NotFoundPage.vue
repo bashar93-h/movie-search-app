8e8b8f46acef59e7645f08bbfe5ef271b716b3b9
Bashar <basharabulaziz25@gmail.com>
2025-11-23 21:49:17 +0300

    add Not found page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex flex-col items-center text-text-light dark:text-text-dark mt-10">
+    <h1 class="text-6xl font-bold mb-4">404</h1>
+    <p class="text-xl mb-6">Oops! The page you're looking for doesn’t exist.</p>
+    <RouterLink
+      to="/"
+      class="py-2 px-3 rounded-md text-white bg-accent-light dark:bg-accent-dark hover:opacity-90 transition"
+      >Go back gome</RouterLink
+    >
+  </div>
+</template>
