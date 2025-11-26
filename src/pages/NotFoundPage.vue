2b04438b4b8b97ca6a0238c818ef6f153f79cadd
Bashar <basharabulaziz25@gmail.com>
2025-11-26 19:18:32 +0300

    update movie search component

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -4,8 +4,8 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <div class="flex flex-col items-center text-text-light dark:text-text-dark mt-10">
-    <h1 class="text-6xl font-bold mb-4">404</h1>
-    <p class="text-xl mb-6">Oops! The page you're looking for doesn’t exist.</p>
+    <h1 class="text-4xl md:text-6xl font-bold mb-4">404</h1>
+    <p class="text-lg md:text-xl mb-6">Oops! The page you're looking for doesn’t exist.</p>
     <RouterLink
       to="/"
       class="py-2 px-3 rounded-md text-white bg-accent-light dark:bg-accent-dark hover:opacity-90 transition"
